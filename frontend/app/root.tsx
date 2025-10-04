ddbec3c4b6778a546d7e84b924f6e8c982721b6f
lucasew <lucas59356@gmail.com>
2025-10-04 09:30:16 -0300

    disable ssr, theme provider copiado da doc
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -9,6 +9,7 @@ import {
 
 import type { Route } from "./+types/root";
 import "./app.css";
+import { ThemeProvider } from "./components/theme-provider";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -34,7 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
 
-        
+
         {children}
         <ScrollRestoration />
         <Scripts />
@@ -44,7 +45,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <ThemeProvider defaultTheme="system" storageKey="theme">
+      <Outlet />
+    </ThemeProvider>
+  )
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
