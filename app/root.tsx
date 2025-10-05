2f6ff98060b2653a2b41aac69dcc1f921d2be0d1
lucasew <lucas59356@gmail.com>
2025-10-05 13:56:41 -0300

    add analytics
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- app/root.tsx
+++ app/root.tsx
@@ -33,7 +33,7 @@ export const links: Route.LinksFunction = () => [
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+  }
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
@@ -44,6 +44,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <script defer src="https://analytics.app.lew.tec.br/script.js" data-website-id="3743fb74-0f0e-437b-802c-2ed1dbbce809"></script>
       </head>
       <body>
 
